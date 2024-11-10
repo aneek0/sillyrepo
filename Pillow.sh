159cfbdc00b1733edc70e7aae215026f75cf5e58
aneek0 <matraskotey@gmail.com>
2024-11-11 00:02:05 +0300

    Update Pillow.sh

--- Pillow.sh
+++ Pillow.sh
@@ -2,14 +2,6 @@
 
 echo -e "\033[2J\033[3;1f"
 
-eval "cat ~/Hikka/assets/download.txt"
-printf "\n\n\033[1;35mHikka is being installed... ✨\033[0m"
-
-echo -e "\n\n\033[0;96mInstalling base packages...\033[0m"
-
-eval "pkg i git libjpeg-turbo openssl -y"
-
-printf "\r\033[K\033[0;32mPackages ready!\e[0m\n"
 echo -e "\033[0;96mInstalling Pillow...\033[0m"
 
 if eval "lscpu | grep Architecture" | grep -qE 'aarch64'; then
