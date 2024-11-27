d26dd514b1b7a674fd2257d5b903eb4e84001749
aneek0 <matraskotey@gmail.com>
2024-11-27 16:15:53 +0300

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,1 @@
+apt update && apt upgrade && apt install git libcairo2 python pip ffmpeg flac -y && git clone https://github.com/coddrago/Hikka && cd Hikka && pip install -r requirements.txt && python3 -m hikka
