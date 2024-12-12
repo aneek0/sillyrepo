5200eeb60d89c4146f49738253ccc9a3b38fb73d
aneek0 <matraskotey@gmail.com>
2024-12-12 23:10:45 +0300

    Update getpy.py

--- getpy.py
+++ getpy.py
@@ -23,23 +23,21 @@ class GetPyFilesMod(loader.Module):
         if not args:
             await utils.answer(message, self.strings("invalid_url", message))
             return
-        
+
         repo_url = args.rstrip("/")
-        api_url = repo_url.replace("github.com", "api.github.com/repos") + "/contents"
+        match = re.match(r"https://github\.com/(?P<owner>[^/]+)/(?P<repo>[^/]+)", repo_url)
+        if not match:
+            await utils.answer(message, self.strings("invalid_url", message))
+            return
+
+        owner = match.group("owner")
+        repo = match.group("repo")
+        api_url = f"https://api.github.com/repos/{owner}/{repo}/contents"
 
         await utils.answer(message, self.strings("fetching_files", message))
 
         try:
-            response = requests.get(api_url)
-            response.raise_for_status()
-
-            data = response.json()
-            py_files = [
-                re.sub(r"\.py$", "", file["name"])
-                for file in data
-                if file["name"].endswith(".py")
-            ]
-
+            py_files = self.fetch_py_files(api_url)
             if py_files:
                 await utils.answer(message, "\n".join(py_files))
             else:
@@ -47,3 +45,17 @@ class GetPyFilesMod(loader.Module):
 
         except requests.RequestException as e:
             await utils.answer(message, f"<b>Произошла ошибка при запросе к GitHub API:</b> <code>{str(e)}</code>")
+
+    def fetch_py_files(self, api_url):
+        """Рекурсивно получает файлы .py из всех директорий репозитория."""
+        py_files = []
+        response = requests.get(api_url)
+        response.raise_for_status()
+
+        data = response.json()
+        for item in data:
+            if item["type"] == "file" and item["name"].endswith(".py"):
+                py_files.append(re.sub(r"\.py$", "", item["name"]))
+            elif item["type"] == "dir":
+                py_files.extend(self.fetch_py_files(item["url"]))
+        return py_files
