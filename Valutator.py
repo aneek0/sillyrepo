f078c679211d1af549be552e9a915c76083de9d2
aneek0 <matraskotey@gmail.com>
2025-08-19 22:17:37 +0300

    Update file

--- Valutator.py
+++ Valutator.py
@@ -18,17 +18,25 @@ class ValutatorMod(loader.Module):
         """
         state = utils.get_args_raw(message)
         chat = "@aneekocurrency_bot"
-        converting_msg = await utils.answer(message, "<emoji document_id=5346192260029489215>💵</emoji> <b>Конвертирую...</b>")
+        
+        # Редактируем исходное сообщение, чтобы показать, что мы работаем
+        await utils.answer(message, "<emoji document_id=5346192260029489215>💵</emoji> <b>Конвертирую...</b>")
         
         async with message.client.conversation(chat) as conv:
             try:
+                # Отправляем запрос боту и ждём ответа
+                # Используем from_users=chat для поиска по имени пользователя
                 response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=6215875699)
+                    events.NewMessage(incoming=True, from_users=chat)
                 )
                 bot_send_message = await message.client.send_message(chat, format(state))
                 bot_response = await response
+                
             except YouBlockedUserError:
-                await converting_msg.edit("<b>Разблокируй</b> " + chat)
+                # Если пользователь заблокировал бота
+                await message.edit("<b>Разблокируй</b> " + chat)
                 return
-            await converting_msg.delete()
-            await message.respond(bot_response.text)
+
+            # После получения ответа от бота, редактируем исходное сообщение
+            # с результатом
+            await message.edit(bot_response.text)
